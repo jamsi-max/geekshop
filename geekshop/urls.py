73540121249e6a5252092eb6fea2060a633d845a
Jony Geek <jonygeek@gmail.com>
2020-02-07 19:50:34 +0300

    Corection style

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.main),
     path('products/', mainapp.products),
-    path('contact', mainapp.contact),
+    path('contact/', mainapp.contact),
 ]
