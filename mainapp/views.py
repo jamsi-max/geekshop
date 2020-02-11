162a40121e7f6a8a5438894d1284557410eba922
jamsi-max <jamsi@bk.ru>
2020-02-12 01:46:37 +0300

    some update cod

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,7 +5,7 @@ import os
 
 def get_data():
     try:
-        data = json.load(open(os.path.abspath('geekshop\data.json'), encoding="utf-8"))
+        data = json.load(open(os.path.abspath(r'geekshop\data.json'), encoding="utf-8"))
     except:
         data = []
     return data
@@ -23,20 +23,5 @@ def products(request):
 def contact(request):
     title = "о нас"
     visit_date = datetime.datetime.now()
-    locations = [
-        {"city": "Москва", "phone": "+7-888-888-8888", "email": "info@geekshop.ru", "address": "В пределах МКАД"},
-        {
-            "city": "Екатеринбург",
-            "phone": "+7-777-777-7777",
-            "email": "info_yekaterinburg@geekshop.ru",
-            "address": "Близко к центру",
-        },
-        {
-            "city": "Владивосток",
-            "phone": "+7-999-999-9999",
-            "email": "info_vladivostok@geekshop.ru",
-            "address": "Близко к океану",
-        },
-    ]
     content = {"title": title, "visit_date": visit_date, "locations": get_data()}
     return render(request, "mainapp/contact.html", content)
