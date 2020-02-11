9f9cf774010911f399d947b5f2a5b6da54e00e21
jamsi-max <jamsi@bk.ru>
2020-02-11 22:00:53 +0300

    Finish all task!!!

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,49 +1,23 @@
 from django.shortcuts import render
 import datetime
 import json
+import os
 
+def get_data():
+    try:
+        data = json.load(open(os.path.abspath('geekshop\data.json'), encoding="utf-8"))
+    except:
+        data = []
+    return data
 
 def main(request):
     title = "главная"
-    products = [
-        {
-        "name": "Отличный стул",
-        "desc": "Расположитесь комфортно.",
-        "image_src": "product-1.jpg",
-        "image_href": "/products/1/",
-        "alt": "продукт 1",
-        },
-        {
-        "name": "Стул повышенного качества",
-        "desc": "Не оторваться.",
-        "image_src": "product-2.jpg",
-        "image_href": "/products/2/",
-        "alt": "продукт 2",
-        },
-    ]
-    content = {"title": title, "products": products}
+    content = {"title": title, "products": get_data()}
     return render(request, 'mainapp/index.html', content)
 
 def products(request):
     title = "продукты"
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-    ]
-    same_products = [
-        {"name": "Отличный стул", "desc": "Не оторваться.", "image_src": "product-11.jpg", "alt": "продукт 11"},
-        {"name": "Стул повышенного качества", "desc": "Комфортно", "image_src": "product-21.jpg", "alt": "продукт 21"},
-        {
-        "name": "Стул премиального качества",
-        "desc": "Просто попробуй.",
-        "image_src": "product-31.jpg",
-        "alt": "продукт 31",
-        },
-    ]
-    content = {"title": title, "links_menu": links_menu, "same_products": same_products}
+    content = {"title": title, "data": get_data()}
     return render(request, 'mainapp/products.html', content)
 
 def contact(request):
@@ -64,5 +38,5 @@ def contact(request):
             "address": "Близко к океану",
         },
     ]
-    content = {"title": title, "visit_date": visit_date, "locations": locations}
+    content = {"title": title, "visit_date": visit_date, "locations": get_data()}
     return render(request, "mainapp/contact.html", content)
