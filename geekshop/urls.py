db34b513670f2f7d7314e98812338215caa46da0
jamsi-max <jamsi@bk.ru>
2020-02-15 02:32:25 +0300

    Create DB and insert data

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -12,21 +12,21 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+""" 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin 
+from django.urls import path, include
+
 import mainapp.views as mainapp
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', mainapp.main, name='main'),
-    path('products/1/', mainapp.products, name='products_1'),
-    path('products/2/', mainapp.products, name='products_2'),
-    path('products/', mainapp.products, name='products'),
-    path('products/all/', mainapp.products, name='products_all'),
-    path('products/home/', mainapp.products, name='products_home'),
-    path('products/office/', mainapp.products, name='products_office'),
-    path('products/modern/', mainapp.products, name='products_modern'),
-    path('products/classic/', mainapp.products, name='products_classic'),
+    path('products/', include("mainapp.urls", namespace="products")),
     path('contact/', mainapp.contact, name='contact'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
